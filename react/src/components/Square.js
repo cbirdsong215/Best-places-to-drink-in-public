8a083db0eb1a5d6060fa00481c6e2f16c4b606a3
birdsongcandice@gmail.com <birdsongcandice@gmail.com>
2017-06-22 17:04:51 -0400

    added index page styling

--- react/src/components/Square.js
+++ react/src/components/Square.js
@@ -5,9 +5,11 @@ class Square extends Component {
     //let squareStyle = {width: 300, height: 200};
     //let imgStyle = {height: 20, width: 20}
     return(
+
       <a href={"/foods/" + this.props.foodId}>
         <img src={this.props.photo}></img>
       </a>
+
     )
   }
 };
