fb9e00c2cdf8f8976a28fa3e3f0b3c30dab34c4c
Phil <philkazan@gmail.com>
2017-06-22 10:31:29 -0400

    added React tests

--- react/test/card.js
+++ react/test/card.js
@@ -4,18 +4,31 @@ import { mount } from 'enzyme';
 import React from 'react';
 
 describe('Component: Card', () => {
-  let wrapper;
+  let wrapper,
+    foodId,
+    photo,
+    name,
+    description;
 
   beforeEach(() => {
     jasmineEnzyme();
     wrapper = mount(
-      <Card/>
+      <Card
+        foodId="1"
+        photo="sample.jpg"
+        name="Awesome Taco"
+        description="This is one awesome taco"
+      />
     );
   });
 
-  it('should render a p tag', () => {
-   expect(wrapper.find('p').length).toEqual(1);
+  it('img tag should be present', () => {
+   expect(wrapper.find('img').toBePresent();
   });
 
-
+  it('should render an img tag with the specific prop', () => {
+    expect(wrapper.find('img').props()).toEqual({
+      src: 'https://foodie-review.s3.amazonaws.com/uploads/food/photo/1/sample.jpg'
+    });
+  });
 });
